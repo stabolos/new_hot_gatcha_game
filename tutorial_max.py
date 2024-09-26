e34d943c50a489642f00986db66ef239841a7305
Zombor02 <tizianbreitkreutz@gmail.com>
2024-09-26 18:09:13 +0200

    movement links rechts

--- tutorial_max.py
+++ tutorial_max.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-from pygame.sprite import _Group
+#from pygame.sprite import _Group
 import sys
 
 pygame.init()
@@ -10,7 +10,7 @@ screen_height = 450
 screen_width = 400
 acceleration = 0.5
 friction = -0.12
-fps = 60
+fps = 240
 
 FramePerSec = pygame.time.Clock()
 
@@ -22,7 +22,31 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.surf = pygame.Surface((30, 30))
         self.surf.fill((128,255,40))
-        self.rect = self.surf.get_rect(center = (10, 420))
+        self.rect = self.surf.get_rect()
+
+        self.pos = vec((10, 385))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+    def move(self):
+        self.acc = vec(0,0)
+    
+        pressed_keys = pygame.key.get_pressed()
+                
+        if pressed_keys[K_a]:
+            self.acc.x = -acceleration
+        if pressed_keys[K_d]:
+            self.acc.x = acceleration 
+        
+        self.acc.x += self.vel.x * friction
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > screen_width:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = screen_width
+        
+        self.rect.midbottom = self.pos
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self):
@@ -34,10 +58,23 @@ class Platform(pygame.sprite.Sprite):
 PT1 = Platform()
 P1 = Player()
 
+all_sprites = pygame.sprite.Group()
+all_sprites.add(PT1)
+all_sprites.add(P1)
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    P1.move()
+    print(P1.pos)
 
+    displaysurface.fill((0,0,0))
+ 
+    for entity in all_sprites:
+        displaysurface.blit(entity.surf, entity.rect)
+ 
     pygame.display.update()
+    FramePerSec.tick(fps)
+
